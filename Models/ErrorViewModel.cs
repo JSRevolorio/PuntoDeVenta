9027ee3c3dea86db1d7c10c6a457b31063b19696
Josue Revolorio <josuerevolorio92@gmail.com>
2020-08-29 12:15:01 -0600

    Inicio de repositorio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SPV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
